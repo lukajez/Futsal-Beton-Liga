574487acd4c76829bffca42cdb3f51dc66113b5a
lukajez <luka.jez55@gmail.com>
2020-05-24 17:47:57 +0200

    Added filters

---
+++ MOSIS_stage_2/app/src/main/java/com/example/mosis/UpdateMatchActivity.java
@@ -0,0 +1,14 @@
+package com.example.mosis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateMatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_match);
+    }
+}
